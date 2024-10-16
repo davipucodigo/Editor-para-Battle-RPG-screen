1da042c5c50316f17f285140aa76b3b3a6e68c99
davipucodigo <davi.santvieira@gmail.com>
2024-10-16 00:11:14 +0000

    players pronto

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //config
 var HP_base = 100;
 //Player
-var valor_HP_p1;
-var valor_HP_p2;
-var valor_HP_p3;
-var valor_HP_p4;
+var valor_HP_p1 = 0;
+var valor_HP_p2 = 0;
+var valor_HP_p3 = 0;
+var valor_HP_p4 = 0;
 
 function Wallpaper_turn() {
     var wp_url;
@@ -62,11 +62,136 @@ function SetHP(num){
             }
             break;
         case 3:
-            
-        break;
+            valor_HP_p3 = prompt("HP player 3:");
+            document.getElementById("hp_p3").innerHTML = "HP: "+valor_HP_p3+"/"+HP_base;
+            if (valor_HP_p3 > 0.7*HP_base) {
+                document.getElementById("hp_p3").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 100%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p3 > 0.5*HP_base && valor_HP_p3 <= 0.7*HP_base) {
+                document.getElementById("hp_p3").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 70%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p3 > 0.2*HP_base && valor_HP_p3 <= 0.5*HP_base) {
+                document.getElementById("hp_p3").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 50%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p3 <= 0.2*HP_base) {
+                document.getElementById("hp_p3").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 20%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p3 <= 0) {
+                document.getElementById("hp_p3").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 0%, rgb(38, 38, 44) 0%)";
+            }
+            break;
         
         case 4:
-            
+            valor_HP_p4 = prompt("HP player 4:");
+            document.getElementById("hp_p4").innerHTML = "HP: "+valor_HP_p4+"/"+HP_base;
+            if (valor_HP_p4 > 0.7*HP_base) {
+                document.getElementById("hp_p4").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 100%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p4 > 0.5*HP_base && valor_HP_p4 <= 0.7*HP_base) {
+                document.getElementById("hp_p4").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 70%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p4 > 0.2*HP_base && valor_HP_p4 <= 0.5*HP_base) {
+                document.getElementById("hp_p4").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 50%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p4 <= 0.2*HP_base) {
+                document.getElementById("hp_p4").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 20%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p4 <= 0) {
+                document.getElementById("hp_p4").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 0%, rgb(38, 38, 44) 0%)";
+            }
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function SetName (idname) {
+    let nome = prompt("Nome player:");
+    if (nome == null || nome == "") {
+        document.getElementById(idname).innerText = "Name player";
+    }else {
+        document.getElementById(idname).innerText = nome;
+    }
+}
+
+function HitPlayer(num) {
+    switch (num) {
+        case 1:
+            valor_HP_p1 -= Number(prompt("Dano no player 1:"));
+            document.getElementById("hp_p1").innerHTML = "HP: "+valor_HP_p1+"/"+HP_base;
+            if (valor_HP_p1 > 0.7*HP_base) {
+                document.getElementById("hp_p1").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 100%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p1 > 0.5*HP_base && valor_HP_p1 <= 0.7*HP_base) {
+                document.getElementById("hp_p1").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 70%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p1 > 0.2*HP_base && valor_HP_p1 <= 0.5*HP_base) {
+                document.getElementById("hp_p1").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 50%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p1 <= 0.2*HP_base) {
+                document.getElementById("hp_p1").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 20%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p1 <= 0) {
+                document.getElementById("hp_p1").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 0%, rgb(38, 38, 44) 0%)";
+            }
+            break;
+        case 2:
+            valor_HP_p2 -= Number(prompt("Dano no player 2:"));
+            document.getElementById("hp_p2").innerHTML = "HP: "+valor_HP_p2+"/"+HP_base;
+            if (valor_HP_p2 > 0.7*HP_base) {
+                document.getElementById("hp_p2").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 100%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p2 > 0.5*HP_base && valor_HP_p2 <= 0.7*HP_base) {
+                document.getElementById("hp_p2").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 70%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p2 > 0.2*HP_base && valor_HP_p2 <= 0.5*HP_base) {
+                document.getElementById("hp_p2").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 50%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p2 <= 0.2*HP_base) {
+                document.getElementById("hp_p2").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 20%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p2 <= 0) {
+                document.getElementById("hp_p2").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 0%, rgb(38, 38, 44) 0%)";
+            }
+            break;
+        case 3:
+            valor_HP_p3 -= Number(prompt("Dano no player 3:"));
+            document.getElementById("hp_p3").innerHTML = "HP: "+valor_HP_p3+"/"+HP_base;
+            if (valor_HP_p3 > 0.7*HP_base) {
+                document.getElementById("hp_p3").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 100%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p3 > 0.5*HP_base && valor_HP_p3 <= 0.7*HP_base) {
+                document.getElementById("hp_p3").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 70%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p3 > 0.2*HP_base && valor_HP_p3 <= 0.5*HP_base) {
+                document.getElementById("hp_p3").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 50%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p3 <= 0.2*HP_base) {
+                document.getElementById("hp_p3").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 20%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p3 <= 0) {
+                document.getElementById("hp_p3").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 0%, rgb(38, 38, 44) 0%)";
+            }
+            break;
+        case 4:
+            valor_HP_p4 -= Number(prompt("Dano no player 4:"));
+            document.getElementById("hp_p4").innerHTML = "HP: "+valor_HP_p4+"/"+HP_base;
+            if (valor_HP_p4 > 0.7*HP_base) {
+                document.getElementById("hp_p4").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 100%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p4 > 0.5*HP_base && valor_HP_p4 <= 0.7*HP_base) {
+                document.getElementById("hp_p4").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 70%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p4 > 0.2*HP_base && valor_HP_p4 <= 0.5*HP_base) {
+                document.getElementById("hp_p4").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 50%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p4 <= 0.2*HP_base) {
+                document.getElementById("hp_p4").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 20%, rgb(38, 38, 44) 0%)";
+            }
+            if (valor_HP_p4 <= 0) {
+                document.getElementById("hp_p4").style.backgroundImage = "linear-gradient(to right, rgb(201, 30, 30) 0%, rgb(38, 38, 44) 0%)";
+            }
             break;
     
         default:
